a71e6efcb040eef9278ffb2e9832aa87bc7f984d
47767573t <Git1234>
2016-03-15 19:51:53 +0100

    Initial commit

---
+++ src/BaseCafeExpreso.java
@@ -0,0 +1,10 @@
+/**
+ * Created by 47767573t on 15/03/16.
+ */
+public class BaseCafeExpreso {
+
+    private String descripcio;
+
+
+    public String getDescripcio() { return descripcio; }
+}
---
+++ src/Beguda.java
@@ -0,0 +1,14 @@
+/**
+ * Created by 47767573t on 15/03/16.
+ */
+
+//DECORATOR
+public abstract class Beguda {
+
+    private String descripcio;
+
+
+    public double preu(){ return 0; }
+
+    public String getDescripcio() { return descripcio; }
+}
